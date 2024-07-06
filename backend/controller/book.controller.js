aba56932538d926a58821839578c83443399bb53
Jashanpreet Singh <jashan.2005preetsingh@gmail.com>
2024-07-06 10:48:53 +0530

    added result saving backend

--- backend/controller/book.controller.js
+++ backend/controller/book.controller.js
@@ -1,5 +1,6 @@
 import Book from "../model/book.model.js";
 import User from "../model/user.model.js";
+import errorHandler from '../utils/error.js'
 
 export const getBook = async (req, res) => {
     try {
@@ -22,5 +23,20 @@ export const getLastFiveScores = async (req, res) => {
     }
     catch (error) {
         return res.status(500).json(error);
+    }
+}
+
+export const saveResult = async (req, res) => {
+    try {
+        const email = req.user.email;
+        const user = User.find({email});
+        const arr = user.scores;
+        const language = req.query.language;
+        const marks = req.query.marks;
+        arr = [...arr, {language: language, score: marks}]
+        return res.status(200).json('saved successfully');
+    } 
+    catch (error) {
+        return next(errorHandler(500, 'failed'));
     }
 }
